00d4898c1bff89fd7c86486539313555efeefd92
eugenesvk <eugenesvk@users.noreply.github.com>
2026-08-17 00:35:20 +0300

    comment telemetry: workspace

--- crates/workspace/src/theme_preview.rs
+++ crates/workspace/src/theme_preview.rs
@@ -93,9 +93,9 @@ impl Item for ThemePreview {
         format!("{} Preview", name).into()
     }
 
-    fn telemetry_event_text(&self) -> Option<&'static str> {
+    /*fn telemetry_event_text(&self) -> Option<&'static str> {
         None
-    }
+    }*/
 
     fn can_split(&self) -> bool {
         true
