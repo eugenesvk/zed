e1d20839c37d385c8e7636ee3647bae4af286b6b
eugenesvk <eugenesvk@users.noreply.github.com>
2026-08-19 02:20:53 +0300

    rules: 4.3 Remove per Directory items @crates/command_palette/

--- crates/command_palette/src/command_palette.rs
+++ crates/command_palette/src/command_palette.rs
@@ -466,7 +466,7 @@ impl PickerDelegate for CommandPaletteDelegate {
         let (mut tx, mut rx) = postage::dispatch::channel(1);
 
         let query_str = query.as_str();
-        let is_zed_link = parse_zed_link(query_str, cx).is_some();
+        
 
         let task = cx.background_spawn({
             let mut commands = self.all_commands.clone();
@@ -499,19 +499,7 @@ impl PickerDelegate for CommandPaletteDelegate {
                 )
                 .await;
 
-                let intercept_result = if is_zed_link {
-                    CommandInterceptResult {
-                        results: vec![CommandInterceptItem {
-                            action: OpenZedUrl {
-                                url: query_for_link.clone().into(),
-                            }
-                            .boxed_clone(),
-                            string: query_for_link,
-                            positions: vec![],
-                        }],
-                        exclusive: false,
-                    }
-                } else if let Some(task) = intercept_task {
+                let intercept_result = if let Some(task) = intercept_task {
                     task.await
                 } else {
                     CommandInterceptResult::default()
