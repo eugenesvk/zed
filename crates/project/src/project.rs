b729235d27229d133fcb75c8f1723448c3a0e46a
eugenesvk <eugenesvk@users.noreply.github.com>
2026-08-19 23:46:41 +0300

    rules: 2.2 Removed refs to Deleted mod telemetry_snapshot @ project from source files

--- crates/project/src/project.rs
+++ crates/project/src/project.rs
@@ -18,7 +18,7 @@ pub mod project_settings;
 pub mod search;
 pub mod task_inventory;
 pub mod task_store;
-pub mod telemetry_snapshot;
+
 pub mod terminals;
 pub mod toolchain_store;
 pub mod trusted_worktrees;
